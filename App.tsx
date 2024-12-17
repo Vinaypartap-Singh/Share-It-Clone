c19861d3b2d6ccf935393dd24a9ddc1f5b0fd0fd
Vinaypartap Singh <77821789+Vinaypartap-Singh@users.noreply.github.com>
2024-12-17 13:40:31 -0500

    Shareit Clone TCP UDP

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { View, Text, Platform } from 'react-native'
+import React, { useEffect } from 'react'
+import Navigation from './src/navigation/Navigation'
+import { requestPhotoPermission } from './src/utils/Constants'
+import { checkFilePermissions } from './src/utils/libraryHelpers'
+
+const App = () => {
+
+  useEffect(()=>{
+    requestPhotoPermission()
+    checkFilePermissions(Platform.OS)
+  },[])
+
+  return (
+    <Navigation />
+  )
+}
+
+export default App
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,30 @@
+import { View, Text, Image } from 'react-native'
+import React, { FC, useEffect } from 'react'
+import { navigate } from '../utils/NavigationUtil'
+import { commonStyles } from '../styles/commonStyles'
+
+const SplashScreen: FC = () => {
+
+    const navigateToHome = () => {
+        navigate('ConnectionScreen')
+    }
+
+    useEffect(() => {
+        const timeoutId = setTimeout(navigateToHome, 1200)
+        return () => clearTimeout(timeoutId)
+    }, [])
+
+
+
+
+    return (
+        <View style={commonStyles.container}>
+            <Image
+                style={commonStyles.img}
+                source={require('../assets/images/logo_text.png')}
+            />
+        </View>
+    )
+}
+
+export default SplashScreen
